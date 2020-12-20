0fd55fc5ba3edb8ca4a19afed9100ee3506e7775
Kezimana Aimé Angelo <kezangelo@gmail.com>
2020-12-20 20:09:08 +0200

    Change route '/collaborators'

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
+use App\Http\Livewire\Collaborators;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CollaboratorController;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::get('/collaborators', [CollaboratorController::class, 'index'])->name('collaborators.index');
+Route::get('/collaborators', Collaborators::class);
